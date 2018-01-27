44bd3bb121f7e424caae28bb2c14e692c53e9c5c
Harris Robin <harrisrobinkalash@gmail.com>
2018-01-27 16:14:37 -0500

    remove references of auth0

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import { GraphQLServer } from "graphql-yoga"
 import { Prisma } from "./generated/prisma"
 import { resolvers, fragmentReplacements } from "./resolvers"
-import checkJWT from "./middleware/check-jwt"
-import getUser from "./middleware/get-user"
 
 const db = new Prisma({
   fragmentReplacements,
@@ -20,18 +18,6 @@ const server = new GraphQLServer({
   }),
 })
 
-// server.express.post(
-//   server.options.endpoint,
-//   checkJWT,
-//   (err, req, res, next) => {
-//     if (err) return res.status(401).send(err.message)
-//     next()
-//   },
-// )
-
-// server.express.post(server.options.endpoint, (req, res, next) =>
-//   getUser(req, res, next, db),
-// )
 
 server.start(({ port }) =>
   console.log(`Server is running on http://localhost:${port}`),
