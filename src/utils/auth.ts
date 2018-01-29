7fa783ce678bdcfe6a3e29e3616f6c0ec9a218c8
Harris Robin <harrisrobinkalash@gmail.com>
2018-01-28 20:11:46 -0500

    remove more irrelevant stuff

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,3 @@
-import * as moment from "moment"
-import { isNil, pickBy, path, ifElse, equals } from "ramda"
 
 export const ctxUser = ctx => ctx.request.user
 
@@ -9,47 +7,14 @@ export const isLoggedIn = ctx => {
 }
 
 export const createPrismaUserFromFacebook = async (ctx, facebookUser) => {
-  const education =
-    facebookUser.education &&
-    facebookUser.education.map(e => ({
-      type: e.type,
-      name: e.school.name,
-    }))
-
-  const work =
-    facebookUser.work &&
-    facebookUser.work.map(w => ({
-      employer: w.employer.name,
-      location: w.location.name,
-      position: w.position.name,
-    }))
-
-  const gender = path(["gender"], facebookUser)
-  const maleOrFemale = gender === "male" ? "MALE" : "FEMALE"
-
-  const userWithKeys = {
-    facebookUserId: facebookUser.id,
-    name: facebookUser.name,
-    first_name: facebookUser.first_name,
-    last_name: facebookUser.last_name,
-    gender: maleOrFemale,
-    locale: facebookUser.locale,
-    email: path(["email"], facebookUser),
-    birthday: moment(path(["birthday"], facebookUser)),
-    education: {
-      create: education,
+  return ctx.db.mutation.createUser({
+    data: {
+      facebookUserId: facebookUser.id,
+      name: facebookUser.name,
+      first_name: facebookUser.first_name,
+      last_name: facebookUser.last_name,
+      locale: facebookUser.locale,
+      birthday: facebookUser.birthday,
     },
-    work: {
-      create: work,
-    },
-    photos: {
-      create: {
-        url: path(["picture", "data", "url"], facebookUser),
-      },
-    },
-    lastLogin: moment(),
-  }
-  var isNotNil = val => !isNil(val)
-  const userWithoutNullKeys = pickBy(isNotNil, userWithKeys)
-  return ctx.db.mutation.createUser({ data: userWithoutNullKeys })
+  })
 }
